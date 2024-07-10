fb531440bd9adfc6a4323fc0e9774dedd1ff36f0
Kishore Selvaraj <89259172+Kishoreselvaraj@users.noreply.github.com>
2024-07-10 14:21:43 +0530

    .

--- routs/router.js
+++ routs/router.js
@@ -3,4 +3,15 @@ const router=express.Router();
 router.route("/").get((req,res)=>{
     res.send("Hi this is home page");
 });
+router.route("/:id").post((req,res)=>{
+    // res.send('New id created ${}');
+    res.status(200).json({message:(req.params.id )});
+});
+router.route("/:id").put((req,res)=>{
+    res.send("Updated a id"(req.params.id));
+});
+router.route("/:id").delete((req,res)=>{
+    res.send("Hi this is home page");
+});
+
 module.exports=router;
