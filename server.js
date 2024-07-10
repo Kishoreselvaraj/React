531ff40156d68162485b90253f16fa0c257b30bb
Kishore Selvaraj <89259172+Kishoreselvaraj@users.noreply.github.com>
2024-07-10 13:54:44 +0530

    request

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("hi");
+const express=require("express");
+const dotenv=require("dotenv").config();
+const app=express();
+const port= process.env.port||5000;
+app.use("/home",require("./routs/router"));
+app.listen(port,()=>{
+    console.log('Server running in port',port);
+});
